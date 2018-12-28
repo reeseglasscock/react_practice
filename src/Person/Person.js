35228350647fd33abab28451e11a02a2c5129baa
reeseglasscock <reesegpdx@gmail.com>
2018-12-27 20:56:13 -0800

    add prompt to put age of person

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const person = () => {
-  return <p>I'm a person!</p>
+  return <p>I'm a person and I am {x} years old!</p>
 }
+
+const x = prompt("How old are you?");
+
 
 export default person;
