25efdc541add74d2a62ab7022cffe5470563ffce
reeseglasscock <reesegpdx@gmail.com>
2018-12-27 21:40:01 -0800

    updating state onclick handler

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 30},
+      { name: 'Peter', age: 13},
+      { name: 'Stacie', age: 44},
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log(`I just go clicked!`)
+    this.setState({
+      persons: [
+        { name: 'Chocolate Lab Brownies', age: 10000 },
+        { name: 'Peter', age: 13 },
+        { name: 'Stacie', age: 44 },
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <h1>Hi, I'm a react app!</h1>
-        <Person name="Peter" age="30"/>
-        <Person name="Max" age="15">I am amazing and have some awesome hobbies like: </Person>
-        <Person name="Stephanie" age="22"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
