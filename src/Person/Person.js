5c9226f9454684acf61ff54a03772c87c922ac01
reeseglasscock <reesegpdx@gmail.com>
2018-12-27 21:07:40 -0800

    dynamically rendering children props with conditional

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-const person = () => {
-  return <p>I'm a person and I am {x} years old!</p>
+const person = (props) => {
+  return (
+    <div>
+      <p>I'm a {props.name} and I am {props.age} years old!</p>
+      {renderChildren(props.children)}
+    </div>
+  )
 }
 
-const x = prompt("How old are you?");
-
+function renderChildren(children) {
+  if (children) {
+    return <p>I am awesome!</p>
+  }
+}
 
 export default person;
