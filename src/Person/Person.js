3639bfd54c14fb5ba7ccdf748bfa977f9ae8a313
reeseglasscock <reesegpdx@gmail.com>
2018-12-30 13:52:31 -0800

    change styles to use modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import './Person.css'
+import Radium from 'radium'
+import styles from './Person.module.css'
 
 const person = (props) => {
+  const style = {
+    '@media (min-width: 500px)' : {
+      width: '450px'
+    }
+  }
   return (
-    <div className="Person">
+    <div className={[styles.red, styles.person].join(' ')} style={style}>
       <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
       {renderChildren(props.children)}
       <input type="text" onChange={props.changed} value={props.name}/>
@@ -17,4 +23,4 @@ const renderChildren = (children) => {
   }
 }
 
-export default person;
+export default Radium(person);
