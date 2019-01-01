53e8e5eae782b3acbaeea7b37ecda6477bd9aaea
reeseglasscock <reesegpdx@gmail.com>
2019-01-01 09:06:22 -0800

    change to stateful components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Radium from 'radium'
 import styles from './Person.module.css'
 
-const person = (props) => {
-  const style = {
-    '@media (min-width: 500px)' : {
-      width: '450px'
+class Person extends Component {
+  render() {
+    const style = {
+      '@media (min-width: 500px)' : {
+        width: '450px'
+      }
     }
+    return (
+      <div className={[styles.red, styles.person].join(' ')} style={style}>
+        <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
+        {renderChildren(this.props.children)}
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
+    )
   }
-  return (
-    <div className={[styles.red, styles.person].join(' ')} style={style}>
-      <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
-      {renderChildren(props.children)}
-      <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-  )
 }
 
 const renderChildren = (children) => {
@@ -23,4 +25,4 @@ const renderChildren = (children) => {
   }
 }
 
-export default Radium(person);
+export default Radium(Person);
