792199e32a063b88533afead4035cbeff9c5e3dd
reeseglasscock <reesegpdx@gmail.com>
2019-01-01 16:08:56 -0800

    use empty tag rather than using a div

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -25,13 +25,15 @@ const cockpit = (props) => {
   }
 
   return (
-    <div className={styles.Cockpit}>
-      <h1>Hi, I'm a react app!</h1>
-      <p className={classes.join(' ')}>This is really working!</p>
-      <button
-        style={style}
-        onClick={props.clicked}>Switch Name</button>
-    </div>
+    <>
+      <div className={styles.Cockpit}>
+        <h1>Hi, I'm a react app!</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+          style={style}
+          onClick={props.clicked}>Switch Name</button>
+      </div>
+    </>
       )
 }
 
